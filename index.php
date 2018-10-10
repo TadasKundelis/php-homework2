c3bb3202ac795ff8fc11c9d75cf5c071a0c4a73b
Kundelis, Tadas <tkundelis@csc.com>
2018-10-10 22:45:34 +0300

    moved index.php out of src

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require '../vendor/autoload.php';
+    require './vendor/autoload.php';
     echo "calculateHomeWorkSum: " . calculateHomeWorkSum(3, 2.2, '1') . "<br />\n";
     echo "Nfq\Akademija\Not_Typed\calculateHomeWorkSum: " . Nfq\Akademija\Not_Typed\calculateHomeWorkSum(3, 2.2, '1') . "<br />\n";
     echo "Nfq\Akademija\Soft\calculateHomeWorkSum: " . Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1') . "<br />\n";
