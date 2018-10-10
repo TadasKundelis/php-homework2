7bbce7c150840699290707fb0371760724159d8b
Kundelis, Tadas <tkundelis@csc.com>
2018-10-10 22:01:57 +0300

    initial commit

---
+++ src/functions_strict.php
@@ -0,0 +1,15 @@
+<?php 
+	declare(strict_types=1);
+	namespace  Nfq\Akademija\Strict{ 
+		function countSum(int...$numbers): int {
+			$total = 0;
+			foreach ($numbers as $num) {
+				$total += $num;
+			}
+			return $total;
+		}
+		function calculateHomeWorkSum(...$numbers) {
+			return countSum(...$numbers);
+		}
+	}
+?>
