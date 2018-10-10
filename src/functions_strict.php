eb1a11ea42d29bfd861a1b85145c639e4af14603
TadasKundelis <kundelist@gmail.com>
2018-10-10 22:41:53 +0300

    Update functions_strict.php

--- src/functions_strict.php
+++ src/functions_strict.php
@@ -1,6 +1,6 @@
 <?php 
     declare(strict_types=1);
-    namespace  Nfq\Akademija\Strict{ 
+    namespace  Nfq\Akademija\Strict { 
         function countSum(int...$numbers): int {
             $total = 0;
             foreach ($numbers as $num) {
